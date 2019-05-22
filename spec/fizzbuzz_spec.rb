e1077f39e9ef89e6f6eeaf61af70692d0d1bdfff
Leon Linton <leon.linton@live.co.uk>
2019-05-22 18:53:10 +0100

    Refactored FizzBuzz Test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,22 +4,19 @@ describe 'fizzbuzz' do
   it 'returns "Fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'Fizz'
   end
-end
 
-describe 'fizzbuzz' do
   it 'returns "Buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'Buzz'
   end
-end
 
-describe 'fizzbuzz' do
-  it 'returns "FizzBuzz" when passed 15' do
+  it 'returns "FizzBuzz" when number is divisble by 3 and 5' do
     expect(fizzbuzz(15)).to eq 'FizzBuzz'
+    expect(fizzbuzz(30)).to eq 'FizzBuzz'
   end
-end
 
-describe 'fizzbuzz' do
-  it 'returns 8 when passed 8' do
+  it 'returns number when number is not divisible by 3 or 5' do
     expect(fizzbuzz(8)).to eq 8
+    expect(fizzbuzz(13)).to eq 13
+    expect(fizzbuzz(19)).to eq 19
   end
 end
